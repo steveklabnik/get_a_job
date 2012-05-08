4e9605a0b190ab919082486724633daccb60b26e
Steve Klabnik <steve@steveklabnik.com>
2012-05-08 15:21:20 -0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,9 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# Make sure your secret_token is kept private
+# if you're sharing your code publicly.
+GetAJob::Application.config.secret_token = 'e9ee2234f9607b508eb6c4229fc2465934a48b667b143c1f6edb18a37d1aec39c2472318b818b3067a51c9685e32f0ce411a52b20fb301e908b713038ac95d2c'
