aa49bde5764df37d85c7d40c53887eb7b04082d5
Steve Klabnik <steve@steveklabnik.com>
2012-11-05 12:38:20 +0100

    Initial commit.
    Zomg edge rails, ember-rails, ams.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_token is kept private
+# if you're sharing your code publicly.
+GetAJob::Application.config.secret_token = '0c7c3875f79d6deb7d9f0064d6a6f046b0089cc5ebb77699eea93de6a3401b0dd4c5acab58dffb07c634f85ddb9dcab35ca4aa004f8be4a99952c4c6b29c4293'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+GetAJob::Application.config.session_store :cookie_store, key: '_get_a_job_session'
